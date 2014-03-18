8ef895ef4daf04a48d8fe4c21adb66f862123070
Tom Arnfeld <tarnfeld@me.com>
2014-03-18 23:08:09 +0000

    Implement example resolvers for A, TXT and CNAME

--- handler.go
+++ handler.go
@@ -8,33 +8,39 @@ func LookupHandler(response dns.ResponseWriter, req *dns.Msg) {
 	q := req.Question[0]
 	r := &Resolver{}
 
-    var answers []dns.RR
+    m := new(dns.Msg)
+	m.SetReply(req)
 
 	if q.Qclass == dns.ClassINET {
 		if q.Qtype == dns.TypeA {
 			logger.Printf("Q: A record for %s", q.Name)
-			answers = r.LookupA(q.Name)
+
+			for _, a := range r.LookupA(q.Name, q.Qclass, q.Qtype) {
+				header := a.Header()
+				logger.Printf("A: %s (TTL %d)", a.A, header.Ttl)
+				m.Answer = append(m.Answer, a)
+			}
 		}
-		// if q.Qtype == dns.TypeTXT {
-		// 	logger.Printf("Q: TXT record for %s", q.Name)
-		// 	answers = r.LookupA(q.Name)
-		// }
-		// if q.Qtype == dns.TypeCNAME {
-		// 	logger.Printf("Q: CNAME record for %s", q.Name)
-		// 	answers = r.LookupA(q.Name)
-		// }
-	}
 
-	m := new(dns.Msg)
-	m.SetReply(req)
+		if q.Qtype == dns.TypeTXT {
+			logger.Printf("Q: TXT record for %s", q.Name)
 
-	for _, a := range answers {
-		header := a.Header()
-		header.Name = q.Name
-		header.Class = q.Qclass
-		header.Rrtype = q.Qtype
+			for _, a := range r.LookupTXT(q.Name, q.Qclass, q.Qtype) {
+				header := a.Header()
+				logger.Printf("A: %s (TTL %d)", a.Txt[0], header.Ttl)
+				m.Answer = append(m.Answer, a)
+			}
+		}
 
-		m.Answer = append(m.Answer, a)
+		if q.Qtype == dns.TypeCNAME {
+			logger.Printf("Q: CNAME record for %s", q.Name)
+
+			for _, a := range r.LookupCNAME(q.Name, q.Qclass, q.Qtype) {
+				header := a.Header()
+				logger.Printf("A: %s (TTL %d)", a.Target, header.Ttl)
+				m.Answer = append(m.Answer, a)
+			}
+		}
 	}
 
 	response.WriteMsg(m)
--- resolver.go
+++ resolver.go
@@ -9,21 +9,32 @@ type Resolver struct {
     zookeeper   string
 }
 
-func (r *Resolver) LookupA(name string) []dns.A {
-    answers := make([]dns.A, 5)
+func (r *Resolver) LookupA(name string, class uint16, rrtype uint16) []*dns.A {
+    answers := make([]*dns.A, 1)
 
-    for i := 0; i < 5; i++ {
-        rr_header := &dns.RR_Header{Ttl: 0}
-        answers[i] = dns.A{*rr_header, net.ParseIP("13.37.13.37")}
-    }
-    
+    rr_header := &dns.RR_Header{Name: name, Class: class, Rrtype: rrtype, Ttl: 0}
+    answers[0] = &dns.A{*rr_header, net.ParseIP("13.37.13.37")}
+
     return answers
 }
 
-func (r *Resolver) LookupTXT(req *dns.Msg, name string) []*dns.TXT {
-    return nil
+func (r *Resolver) LookupTXT(name string, class uint16, rrtype uint16) []*dns.TXT {
+    answers := make([]*dns.TXT, 1)
+
+    text := make([]string, 1)
+    text[0] = "testing"
+
+    rr_header := &dns.RR_Header{Name: name, Class: class, Rrtype: rrtype, Ttl: 0}
+    answers[0] = &dns.TXT{*rr_header, text}
+
+    return answers
 }
 
-func (r *Resolver) LookupCNAME(req *dns.Msg, name string) []*dns.CNAME {
-    return nil
+func (r *Resolver) LookupCNAME(name string, class uint16, rrtype uint16) []*dns.CNAME {
+    answers := make([]*dns.CNAME, 1)
+
+    rr_header := &dns.RR_Header{Name: name, Class: class, Rrtype: rrtype, Ttl: 0}
+    answers[0] = &dns.CNAME{*rr_header, "duedil.com."}
+
+    return answers
 }
