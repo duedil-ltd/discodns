683576dcd7f40c4d6708276f5067d0ef4677d57d
Tom Arnfeld <tarnfeld@me.com>
2014-11-21 15:03:50 +0000

    Added some basic test cases for panic based locks

--- lock_test.go
+++ lock_test.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+    "testing"
+)
+
+func TestLock(t *testing.T) {
+    // Enable debug logging
+    log_debug = true
+}
+
+func TestSimpleLockUnlock(t *testing.T) {
+    lock := lockDomain(client, "discodns.net")
+
+    if lock.IsLocked() != true {
+        t.Error("Expected lock to be locked, it was not")
+        t.Fatal()
+    }
+
+    lock.Unlock(false)
+
+    if lock.IsLocked() != false {
+        t.Error("Expected lock to be unlocked, it was not")
+        t.Fatal()
+    }
+}
+
+func TestConflictingLock(t *testing.T) {
+    lockA := lockDomain(client, "discodns.net")
+    if lockA.IsLocked() != true {
+        t.Error("Expected lock to be locked, it was not")
+        t.Fatal()
+    }
+
+    // Defer a function to handle the panic when we request overlapping
+    // locks. We do this here because if the lock fails to be acquired above
+    // the test should fail exceptionally.
+    defer func() {
+        p := recover()
+        if p == nil {
+            t.Error("Expected a panic, got nil")
+            t.Fatal()
+        }
+    }()
+
+    lockDomain(client, "discodns.net")
+}
