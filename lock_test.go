dd801398247daf888460a3ac05e48c0eab79de47
Owen Smith <owen@duedil.com>
2015-02-18 00:21:26 +0000

    Add simple lock test

--- lock_test.go
+++ lock_test.go
@@ -1,5 +1,32 @@
 package main
 
-// import (
-//     "testing"
-// )
+import (
+    "testing"
+    "time"
+)
+
+func TestSimpleLockUnlock(t *testing.T) {
+    testKey := "TestSimpleLockUnlock/.lock"
+    client.Delete(testKey, true)
+
+    lock := NewEtcdKeyLock(client, testKey)
+    locked, err := lock.WaitForAcquire(1)
+
+    if !locked || err != nil {
+        t.Error("Expected to acquire lock, failed")
+        t.Fatal()
+    }
+    _, err = client.Get(testKey, false, true)
+    if err != nil {
+        t.Error("Lock claimed to succeed but etcd record missing/broken")
+        t.Fatal()
+    }
+
+    lock.Abandon()
+    time.Sleep(500 * time.Millisecond)
+    _, err = client.Get(testKey, false, true)
+    if err == nil {
+        t.Error("Lock abandoned, but key exists")
+        t.Fatal()
+    }
+}
