36a19cc255315b96a4f12979bc3cad98cc9a2aba
Owen Smith <owen@duedil.com>
2015-02-23 16:29:05 +0000

    Tests for Name is/is not in use

--- update_test.go
+++ update_test.go
@@ -113,3 +113,105 @@ func TestInsertMultipleRecords(t *testing.T) {
         t.Fatal()
     }
 }
+
+func TestPrerequisites_NameInUse(t *testing.T) {
+    manager := &DynamicUpdateManager{etcd: client, etcdPrefix: "TestPrerequisites_NameInUse/", resolver: resolver}
+    resolver.etcdPrefix = manager.etcdPrefix
+
+    client.Delete("TestPrerequisites_NameInUse/", true)
+    client.Set("TestPrerequisites_NameInUse/net/disco/foo/.A", "1.1.1.1", 0)
+
+    recordToAdd := &dns.A{
+        Hdr: dns.RR_Header{Name: "bar.disco.net.", Rrtype: dns.TypeA, Class: dns.ClassINET},
+        A: net.ParseIP("1.2.3.4")}
+
+    prereq_fail := &dns.ANY{ Hdr: dns.RR_Header{Name: "foofoo.disco.net.", Rrtype: dns.TypeANY, Class: dns.ClassINET}}
+    prereq_ok := &dns.ANY{ Hdr: dns.RR_Header{Name: "foo.disco.net.", Rrtype: dns.TypeANY, Class: dns.ClassINET}}
+
+    msg_1 := &dns.Msg{}
+    msg_1.Question = append(msg_1.Question, dns.Question{Name: "bar.disco.net."})
+    msg_1.Insert([]dns.RR{recordToAdd})
+    msg_1.NameUsed([]dns.RR{prereq_fail})
+
+    result_1 := manager.Update("disco.net.", msg_1)
+    if result_1.Rcode != dns.RcodeNameError {
+        debugMsg(result_1)
+        t.Error("expected update to fail with NXDOMAIN, actually got", dns.RcodeToString[result_1.Rcode])
+        t.Fatal()
+    }
+
+    msg_2 := &dns.Msg{}
+    msg_2.Question = append(msg_2.Question, dns.Question{Name: "bar.disco.net."})
+    msg_2.Insert([]dns.RR{recordToAdd})
+    msg_2.NameUsed([]dns.RR{prereq_fail, prereq_ok})
+
+    result_2 := manager.Update("disco.net.", msg_2)
+    if result_2.Rcode != dns.RcodeNameError {
+        debugMsg(result_2)
+        t.Error("expected update to fail with NXDOMAIN, actually got", dns.RcodeToString[result_2.Rcode])
+        t.Fatal()
+    }
+
+    msg_3 := &dns.Msg{}
+    msg_3.Question = append(msg_3.Question, dns.Question{Name: "bar.disco.net."})
+    msg_3.Insert([]dns.RR{recordToAdd})
+    msg_3.NameUsed([]dns.RR{prereq_ok})
+
+    result_3 := manager.Update("disco.net.", msg_3)
+    if result_3.Rcode != dns.RcodeSuccess {
+        debugMsg(result_3)
+        t.Error("Failed to add DNS record with name-in-use prereq, got", dns.RcodeToString[result_3.Rcode])
+        t.Fatal()
+    }
+}
+
+func TestPrerequisites_NameNotInUse(t *testing.T) {
+    manager := &DynamicUpdateManager{etcd: client, etcdPrefix: "TestPrerequisites_NameNotInUse/", resolver: resolver}
+    resolver.etcdPrefix = manager.etcdPrefix
+
+    client.Delete("TestPrerequisites_NameNotInUse/", true)
+    client.Set("TestPrerequisites_NameNotInUse/net/disco/foo/.A", "1.1.1.1", 0)
+
+    recordToAdd := &dns.A{
+        Hdr: dns.RR_Header{Name: "bar.disco.net.", Rrtype: dns.TypeA, Class: dns.ClassINET},
+        A: net.ParseIP("1.2.3.4")}
+
+    prereq_fail := &dns.ANY{ Hdr: dns.RR_Header{Name: "foo.disco.net.", Rrtype: dns.TypeANY, Class: dns.ClassINET}}
+    prereq_ok := &dns.ANY{ Hdr: dns.RR_Header{Name: "foofoo.disco.net.", Rrtype: dns.TypeANY, Class: dns.ClassINET}}
+
+    msg_1 := &dns.Msg{}
+    msg_1.Question = append(msg_1.Question, dns.Question{Name: "bar.disco.net."})
+    msg_1.Insert([]dns.RR{recordToAdd})
+    msg_1.NameNotUsed([]dns.RR{prereq_fail})
+
+    result_1 := manager.Update("disco.net.", msg_1)
+    if result_1.Rcode != dns.RcodeYXDomain {
+        debugMsg(result_1)
+        t.Error("expected update to fail with RcodeYXDomain, actually got", dns.RcodeToString[result_1.Rcode])
+        t.Fatal()
+    }
+
+    msg_2 := &dns.Msg{}
+    msg_2.Question = append(msg_2.Question, dns.Question{Name: "bar.disco.net."})
+    msg_2.Insert([]dns.RR{recordToAdd})
+    msg_2.NameNotUsed([]dns.RR{prereq_fail, prereq_ok})
+
+    result_2 := manager.Update("disco.net.", msg_2)
+    if result_2.Rcode != dns.RcodeYXDomain {
+        debugMsg(result_2)
+        t.Error("expected update to fail with RcodeYXDomain, actually got", dns.RcodeToString[result_2.Rcode])
+        t.Fatal()
+    }
+
+    msg_3 := &dns.Msg{}
+    msg_3.Question = append(msg_3.Question, dns.Question{Name: "bar.disco.net."})
+    msg_3.Insert([]dns.RR{recordToAdd})
+    msg_3.NameNotUsed([]dns.RR{prereq_ok})
+
+    result_3 := manager.Update("disco.net.", msg_3)
+    if result_3.Rcode != dns.RcodeSuccess {
+        debugMsg(result_3)
+        t.Error("Failed to add DNS record with name-not-in-use prereq, got", dns.RcodeToString[result_3.Rcode])
+        t.Fatal()
+    }
+}
