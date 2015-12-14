c8e3c2cdf5801dc535852fec271a2f99b5c764cd
Soren Mathiasen <smo@tradeshift.com>
2015-12-14 11:12:20 +0100

    Ran gofmt

--- error.go
+++ error.go
@@ -1,33 +1,34 @@
 package main
 
 import (
-    "github.com/coreos/go-etcd/etcd"
-    "github.com/miekg/dns"
-    "fmt"
+	"fmt"
+	"github.com/coreos/go-etcd/etcd"
+	"github.com/miekg/dns"
 )
 
 type NodeConversionError struct {
-    Message string
-    Node *etcd.Node
-    AttemptedType uint16
+	Message       string
+	Node          *etcd.Node
+	AttemptedType uint16
 }
 
 func (e *NodeConversionError) Error() string {
-    return fmt.Sprintf(
-        "Unable to convert etc Node into a RR of type %d ('%s'): %s. Node details: %+v",
-        e.AttemptedType,
-        dns.TypeToString[e.AttemptedType],
-        e.Message,
-        &e.Node)
+	return fmt.Sprintf(
+		"Unable to convert etc Node into a RR of type %d ('%s'): %s. Node details: %+v",
+		e.AttemptedType,
+		dns.TypeToString[e.AttemptedType],
+		e.Message,
+		&e.Node)
 }
 
 type RecordValueError struct {
-    Message string
-    AttemptedType uint16
+	Message       string
+	AttemptedType uint16
 }
+
 func (e *RecordValueError) Error() string {
-    return fmt.Sprintf(
-        "Invalid record value for type %d: %s",
-        e.AttemptedType,
-        e.Message)
+	return fmt.Sprintf(
+		"Invalid record value for type %d: %s",
+		e.AttemptedType,
+		e.Message)
 }
